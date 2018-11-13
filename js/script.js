3cf7ca88685ac88040170d05bb5b878078f001f5
Igor Todorovic <igor333m@yahoo.com>
2018-11-12 22:06:01 -0800

    Remove FCC data

--- js/script.js
+++ js/script.js
@@ -57,7 +57,9 @@ const weatherData = {
 
 
 
-const element = document.getElementById("main"),
+const main = document.getElementById("main"),
+  cityName = document.getElementById("city-name"),
+  cityPopulation = document.getElementById("city-population"),
   dayInWeek = document.getElementsByClassName("dayInWeek")[0],
   wetherIcon = document.getElementsByClassName("wetherIcon")[0],
   temperature = document.getElementsByClassName("temperature")[0],
@@ -70,25 +72,22 @@ const element = document.getElementById("main"),
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(function(position) {
 
-    let openWeather5dayData = null;
-
     const curentPosition = {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     }
 
-
     /*
-    * Free code camp Weather api
-    * Fcc endpoint example 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139
+    * Openweathermap.org api
+    * Openweathermap today endpoint example "http://api.openweathermap.org/data/2.5/weather?lat=113.2&lon=23.6&APPID=111111111111111111111"
     * Fetch data with lat&lon
     */
-    const urlWeatherMap = 'https://fcc-weather-api.glitch.me/api/current'; //?lat=35&lon=139
+    const urlWeatherMap = 'https://api.openweathermap.org/data/2.5/weather'; //?lat=35&lon=139
 
-    fetch(`${urlWeatherMap}?lat=${curentPosition.latitude}&lon=${curentPosition.longitude}`)
+    fetch(`${urlWeatherMap}?lat=${curentPosition.latitude}&lon=${curentPosition.longitude}&APPID=799d024701320e733102ddfe7106d53c`)
       .then(data => {return data.json()})
-      .then(results => {
-        showToday(results);
+      .then(openWeatherToday => {
+        showToday(openWeatherToday);
       })
       .catch(err => console.log(err))
 
@@ -103,42 +102,43 @@ if ("geolocation" in navigator) {
       .then(data => {return data.json()})
       .then(results => {
         console.log("Open Weather data", results);
-        openWeather5dayData = results;
+        //openWeather5dayData = results;
       })
       .catch(err => console.log(err))
+
+      
   })
 } else {
   alert('geolocation IS NOT available');
 }
 
-function showToday(weatherData) {
-  // console.log("Fcc data: ", weatherData);
+function showToday(openWeatherToday) {
   const date = new Date();
-
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  console.log("openWeatherToday: ", openWeatherToday);
   const today = days[date.getDay()];
-  const currentTemp = Math.round(weatherData.main.temp);
-  const windSpeed = weatherData.wind.speed;
-  const windDirections = weatherData.wind.deg;
-  //openModal(today, currentTemp, windSpeed, windDirections)
-
+  const currentTemp = Math.round(openWeatherToday.main.temp);
+  const windSpeed = openWeatherToday.wind.speed;
+  const windDirections = openWeatherToday.wind.deg;
+  cityName.innerHTML = openWeatherToday.name;
+  main.innerHTML = `<span class="today">${today}</span><span class="current-temp">${currentTemp}</span><span class="wind-speed">${windSpeed}</span><span class="wind-directions">${windDirections}</span>`;
 }
 
 console.log("currentTemp: ", Math.round(currentTemp));
 
 
-openWeather5dayData.list.week.forEach(day => {
-  const eachDay = document.createElement("li");
-  eachDay.classList.add(day.day);
+// openWeather5dayData.list.week.forEach(day => {
+//   const eachDay = document.createElement("li");
+//   eachDay.classList.add(day.day);
 
-  eachDay.addEventListener("click", function() {
-  	windArrow(day.windDirection);
-  	openModal(day.day, day.type, day.temp, day.windSpeed, day.windDirection);
-  });
+//   eachDay.addEventListener("click", function() {
+//   	windArrow(day.windDirection);
+//   	openModal(day.day, day.type, day.temp, day.windSpeed, day.windDirection);
+//   });
   
-  eachDay.innerHTML = `<span class='day'>${day.dt}</span><span class='temp'>${day.main.temp}</span>`;
-  element.appendChild(eachDay);
-});
+//   eachDay.innerHTML = `<span class='day'>${day.dt}</span><span class='temp'>${day.main.temp}</span>`;
+//   main.appendChild(eachDay);
+// });
 
 function windArrow(direction) {
   if (direction === "north-east") {
